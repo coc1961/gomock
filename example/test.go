fa7adf6085e21d3f337c2d0f6872d5cdd727be8b
Carlos Castro <carlos_o_castro@yahoo.com.ar>
2022-09-18 17:10:03 -0300

    Add Test

--- example/test.go
+++ example/test.go
@@ -18,4 +18,5 @@ type TestInterface interface {
 	Func3(str1, str2 string) error
 	Func2(arr []int) (IFace, t1.T1Interface, map[string]string, error)
 	Func1(i int, pt1 t1.T1) (rt1 *t1.T1, xx t1.T1Interface, err error)
+	Func0(string)
 }
